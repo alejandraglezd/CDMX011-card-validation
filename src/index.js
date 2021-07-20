f3740a85295cdd6b43a05ad34abd9802b2e44afb
alejandraglezd <alejandra.gnzlz.dz@gmail.com>
2021-07-19 22:30:44 -0500

    segundo commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import validator from './validator.js';
+/*Constante para traer los valores ingresados al clickear el botón Validar*/
+const validateBtn = document.getElementById("btnValidar");
+/*Agregamos un evento de escucha al dar click sobre el btn que aplicará la función declarada enseguida*/
+validateBtn.addEventListener("click", function() {
+    /*Variable para traer de html los valores ingresados en el input validateNumber*/
+    let cardNumber = document.getElementById("validateNumber").value;
+    /*console.log(cardNumber);*/
+    /*Constante  */
+    const result = validator.isValid(cardNumber);
+    /*console.log(result);*/
+    const numberMask = validator.maskify(cardNumber);
+    if (result == true) {
+        const mask = document.getElementById("validateNumber").value = numberMask
+        document.getElementById("finalResult").innerHTML =
+            ("Número de tarjeta válido" + mask)
 
-console.log(validator);
-
-function validar(){
-    
-}
+    } else {
+        const mask = document.getElementById("validateNumber").value = numberMask
+        document.getElementById("finalResult").innerHTML =
+            ("Número de tarjeta inválido" + mask)
+    }
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,46 @@
 const validator = {
-  // ...
-};
-
+    "isValid": function(algoritmo) {
+        /*Creamos nueva variable para un nuevo arreglo; con metedos para que; itere, tenga un orden inverso y trate los valores como números*/
+        let array = Array.from(algoritmo).reverse().map(Number)
+            /*console.log(array);*/
+            /*Variable para el total sea igual a 0 */
+        let total = 0
+            /*Creamos bucle, variable número igual a cero */
+        for (let i = 0; i < array.length; i++) {
+            /*Condicional; si el residuo */
+            if (i % 2 === 1) {
+                /*Variable número doble será igual al arreglo del número multiplicado por dos*/
+                let numeroDoble = array[i] * 2
+                    /*Condicional; si el número doble es mayor o igual a 10*/
+                if (numeroDoble >= 10) {
+                    let sumaNumero = numeroDoble - 9
+                    total += sumaNumero
+                } else {
+                    total += numeroDoble;
+                }
+            } else {
+                total += array[i];
+            }
+        }
+        let resultado = total % 10
+        if (resultado == 0) {
+            return true
+        } else {
+            return false
+        }
+    },
+    "maskify": function(hideNum) {
+        let originalNum = hideNum.length;
+        if (originalNum <= 4) {
+            return hideNum
+        } else {
+            let cardNum = Array.from(hideNum)
+            for (let i = 0; i < hideNum.length - 4; i++) {
+                cardNum[i] = "#"
+            }
+            let finalNum = cardNum.join("")
+            return finalNum
+        }
+    }
+}
 export default validator;
