d360d6211cf553f5f65aa11e93fd4365e4a2995b
alejandraglezd <alejandra.gnzlz.dz@gmail.com>
2021-07-20 01:00:48 -0500

    tercer commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import validator from './validator.js';
-/*Constante para traer los valores ingresados al clickear el botón Validar*/
+/*Constante para traer los valores ingresados en el input al clickear el botón Validar*/
 const validateBtn = document.getElementById("btnValidar");
 /*Agregamos un evento de escucha al dar click sobre el btn que aplicará la función declarada enseguida*/
 validateBtn.addEventListener("click", function() {
@@ -9,12 +9,16 @@ validateBtn.addEventListener("click", function() {
     /*Constante  */
     const result = validator.isValid(cardNumber);
     /*console.log(result);*/
+    /*Constante para el número que queremos enmascarar; será indicado por la propiedad maskify que ya desarrollamos con los valores de cardNumber */
     const numberMask = validator.maskify(cardNumber);
+    /*Condicional; siendo el resultado igual a verdadero*/
     if (result == true) {
+        /*Constante mask será igual al valor de ValidateNumber igual a la constante numberMask*/
         const mask = document.getElementById("validateNumber").value = numberMask
+            /*Obtener el resultado final de html y retornar un mensaje de validación al usuario */
         document.getElementById("finalResult").innerHTML =
             ("Número de tarjeta válido" + mask)
-
+            /*De lo contrario realizar la misma acción pero retornar mensaje con invalidación*/
     } else {
         const mask = document.getElementById("validateNumber").value = numberMask
         document.getElementById("finalResult").innerHTML =
--- src/validator.js
+++ src/validator.js
@@ -5,7 +5,7 @@ const validator = {
             /*console.log(array);*/
             /*Variable para el total sea igual a 0 */
         let total = 0
-            /*Creamos bucle, variable número igual a cero */
+            /*Creamos bucle, variable número igual a cero, número menor al largo del arreglo, recorrer cada posición */
         for (let i = 0; i < array.length; i++) {
             /*Condicional; si el residuo */
             if (i % 2 === 1) {
@@ -29,12 +29,17 @@ const validator = {
             return false
         }
     },
+    /*Declaramos funcion hideHum con la propiedad para enmascarar */
     "maskify": function(hideNum) {
+        /*Variable originalNum será igual al largo de hideNum */
         let originalNum = hideNum.length;
+        /*Condicional; si originalNum es menor o igual que 4 retornamos hideNum y se enmascaran los dugitos */
         if (originalNum <= 4) {
             return hideNum
+                /*De lo contrario, variable cardNumb será igual al arreglo de hideNum */
         } else {
             let cardNum = Array.from(hideNum)
+                /*Creamos bucle;  */
             for (let i = 0; i < hideNum.length - 4; i++) {
                 cardNum[i] = "#"
             }
